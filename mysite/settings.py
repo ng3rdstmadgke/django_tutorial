dc0c70a3d6c27cdef13d3a9f6d33eb983a8945e4
ng3rdstmadgke <ng3rdstmadgke@gmail.com>
2021-09-30 09:03:45 +0000

    pollアプリ

--- mysite/settings.py
+++ mysite/settings.py
@@ -110,6 +110,19 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+    }
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
